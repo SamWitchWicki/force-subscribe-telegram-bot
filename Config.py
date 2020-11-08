1e583422af3ca2171fda5ef24b0f3d6e70cb1e92
KGK06 <72903548+KGK06@users.noreply.github.com>
2020-11-08 14:20:12 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -12,7 +12,7 @@ class Config():
     SUDO_USERS = list(set(SUDO_USERS))
   else:
     BOT_TOKEN = "1495687343:AAGCoy-d78VTAJbFrf0nwae-931WUChSYh0"
-    DATABASE_URL = "mongodb+srv://forcesub:forcesub@cluster0.spi3a.mongodb.net/forcesub?retryWrites=true&w=majority"
+    DATABASE_URL = "postgres://neguejinwptvwe:8ce7b494e5739b0f345a4d1616b5360b72dba2a9db2d983e9fc52437f6b8b55f@ec2-54-237-155-151.compute-1.amazonaws.com:5432/d2ok8hepk27uub"
     APP_ID = "1947467"
     API_HASH = "eb83224cd16cb106f89f7f8970648a52"
     SUDO_USERS = list(set(int(x) for x in ''.split()))

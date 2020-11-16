93bd4ffc7eeeb954d1531853dea6dd5dc4e09ddb
KGK06 <72903548+KGK06@users.noreply.github.com>
2020-11-16 06:40:25 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 def _start(client, message):
     client.send_message(message.chat.id,
         text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
-        parse_mode="html",
+        parse_mode="markdown",
         reply_to_message_id=message.message_id
         )
 
